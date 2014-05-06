2e1db8a9c530b0a37a33bece5dffa73557309bdc
Matt Ward <ward.matt@gmail.com>
2014-05-06 17:25:15 +0100

    Add threaded test for Lazy<T>.Value.

---
+++ LazyTests/ThreadedLazyTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using System.Threading;
+
+namespace LazyTests
+{
+	[TestFixture]
+	public class ThreadedLazyTests
+	{
+		[Test]
+		public void LazyValueCalledBySecondThreadWhilstFirstThreadInLock ()
+		{
+			var goEvent = new AutoResetEvent (false);
+
+			var lazy = new Lazy<string> (() => {
+				goEvent.Set ();
+				Thread.Sleep (2000);
+				throw new Exception ("Lazy exception");
+			});
+
+			Exception firstLazyEx = null;
+			var thread = new Thread (() => {
+				try {
+					string value = lazy.Value;
+				} catch (Exception ex) {
+					firstLazyEx = ex;
+				}
+			});
+			thread.Start ();
+
+			goEvent.WaitOne ();
+			Exception secondLazyEx = null;
+			try {
+				string value = lazy.Value;
+			} catch (Exception ex) {
+				secondLazyEx = ex;
+			}
+
+			Assert.AreEqual ("Lazy exception", secondLazyEx.Message);
+			Assert.AreEqual ("Lazy exception", firstLazyEx.Message);
+			Assert.AreSame (firstLazyEx, secondLazyEx);
+		}
+	}
+}
+
