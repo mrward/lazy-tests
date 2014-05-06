1cb293fd0dee904f821e6d94838c7f55d0148ebc
Matt Ward <ward.matt@gmail.com>
2014-05-06 15:15:07 +0100

    Add Lazy<T>.Value test.

---
+++ LazyTests/LazyTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LazyTests
+{
+	[TestFixture]
+	public class LazyTests
+	{
+		[Test]
+		public void UseLazyValueTwiceWhenLazyFactoryThrowsException ()
+		{
+			var lazy = new Lazy<string>(() => {
+				throw new Exception("My lazy error message");
+			});
+
+			string value;
+			try {
+				value = lazy.Value;
+			} catch {
+			}
+
+			Exception lazyEx = null;
+			try {
+				value = lazy.Value;
+			} catch (Exception ex) {
+				lazyEx = ex;
+			}
+
+			Assert.AreEqual ("My lazy error message", lazyEx.Message);
+		}
+	}
+}
+
